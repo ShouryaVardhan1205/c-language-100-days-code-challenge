8bb77515541a54d115b89b584dc67291d2640753
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2025-08-26 15:02:13 +0530

    Update Q1_day1.c

--- Q1_day1.c
+++ Q1_day1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    float a, b; // Declared two variables for taking the input of numbers: a and b.
-    printf("Enter a:");
-    scanf("%f", &a);
-    printf("Enter b:");
-    scanf("%f", &b);
-    printf("The sum of the given numbers a and b is:%f", a + b);
+    float num1, num2; 
+    printf("Enter num1:");
+    scanf("%f", &num1);
+    printf("Enter num2:");
+    scanf("%f", &num2);
+    printf("The sum of the given numbers a and b is:%f", num1 + num2);
     return 0;
 }
