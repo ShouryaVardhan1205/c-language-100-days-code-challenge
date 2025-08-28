f8acf259267e58a302595048cbab4cdc630f52a8
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2025-08-28 17:28:23 +0530

    Update Q1_day1.c

--- Q1_day1.c
+++ Q1_day1.c
@@ -1,3 +1,18 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
 #include <stdio.h>
 int main()
 {
@@ -9,3 +24,4 @@ int main()
     printf("The sum of the given numbers a and b is:%f", num1 + num2);
     return 0;
 }
+
