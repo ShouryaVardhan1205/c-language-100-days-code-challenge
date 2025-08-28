4a403ed423d9b2bf3f8f608a150d2eb9f3918ff0
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2025-08-28 17:31:40 +0530

    Update Q5_day3.c

--- Q5_day3.c
+++ Q5_day3.c
@@ -1,3 +1,18 @@
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
 #include<stdio.h>
     int main()
     {
@@ -10,4 +25,5 @@
      printf("The value in fahrenheit is %.2f",f);
            // Here f represents degree fahrenheit.
         return 0;
+
     }
